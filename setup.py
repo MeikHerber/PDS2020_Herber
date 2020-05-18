f1fc17dffd6637b4aef58ef8d1025f91b7ea5aa6
Meik Herber <meikherber@gmx.de>
2020-05-18 13:38:48 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='PDS_Herber',
+    version='0.0.1',
+    description="Programming Data Science",
+    author="Meik Herber",
+    author_email="meikherber@gmx.de",
+    packages=["nextbike"],
+    install_requires=[]
+)
